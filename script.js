06c17f2040cdc9e65e1c49aa763abdc68ebe3ad9
jayaram2005 <jayaramsakthip2@gmail.com>
2026-01-09 15:17:40 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const addBtn = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
-// Add task
+
 addBtn.addEventListener("click", () => {
     const taskText = taskInput.value.trim();
 
@@ -14,18 +14,18 @@ addBtn.addEventListener("click", () => {
     const li = document.createElement("li");
     li.textContent = taskText;
 
-    // Toggle completed task
+    
     li.addEventListener("click", () => {
         li.classList.toggle("completed");
     });
 
-    // Delete button
+    
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "X";
     deleteBtn.className = "delete-btn";
 
     deleteBtn.addEventListener("click", (e) => {
-        e.stopPropagation(); // Prevent toggle
+        e.stopPropagation(); 
         li.remove();
     });
 
